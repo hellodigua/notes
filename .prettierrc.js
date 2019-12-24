72af8f8d1c8f075a258cdb1c1eeb47042a8eac66
= <hellodigua@gmail.com>
2019-12-25 00:20:39 +0800

    feat: 初始化package

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// 参考 https://prettier.io/docs/en/options.html
+
+module.exports = {
+  // 行最大长度
+  printWidth: 100,
+  // 使用单引号 是
+  singleQuote: true,
+  // 定义换行符 lf linux-style
+  endOfLine: 'lf'
+};
